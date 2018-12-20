9de4f8fdbc3b5402d7b62522128b771601bb575b
Valentine Vandamme <valentine.vandamme@student.howest.be>
2018-12-20 11:06:22 +0100

    Import Unity

---
+++ Assets/Scripts/IdleToCrouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleToCrouch : MonoBehaviour {
+
+    public Animator Animator;
+    public float InputX;
+    public float InputY;
+
+
+	// Use this for initialization
+	void Start () {
+
+        //get the animator
+        Animator = this.gameObject.GetComponent<Animator>();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        InputY = Input.GetAxis("Vertical");
+        InputX = Input.GetAxis("Horizontal");
+
+        Animator.SetFloat("InputY", InputY);
+        Animator.SetFloat("InputX", InputX);
+
+		
+	}
+}
