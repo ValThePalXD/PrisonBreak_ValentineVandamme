201c9ca90ca7a9b8613b9053f18af8c7576b9d6f
Valentine Vandamme <valentine.vandamme@student.howest.be>
2019-01-11 12:10:13 +0100

    Trying out Climbing animation

---
+++ Unity/Assets/Scripts/ClimbOnBlock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+
+public class ClimbOnBlock : MonoBehaviour
+{
+    //public GameObject Player;
+    public Animator ClimbingAnimation;
+
+    private void Start()
+    {
+        ClimbingAnimation = GetComponent<Animator>();
+            
+            
+    }
+
+
+
+    //void OnTriggerStay()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.E))
+    //    {
+    //        ClimbingAnimation.Play ("Climbing");
+    //        Debug.Log("E pressed");
+    //    }
+    //}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.name == "Player")
+        other.gameObject.GetComponent<Animator>().SetTrigger("IsClimbing");
+    }
+}
